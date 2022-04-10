9e94ef73121d028af1b7f0c404ef10a89aa74eb1
Amber Frantz <amber.k.frantz@mgail.com>
2022-04-10 15:47:43 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+/*function totalBatteries(array) {
+  let totalPrice = 0;
+
+  for (const element of array) {
+    totalPrice += element;
+  };
+
+  return totalPrice;
+}
+*/
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator}, 0)
+
+
+/*
+const sinon = require( 'sinon' )
+
+describe('reducer', function() {
+  describe('batteries', function() {
+    it('should have a `totalBatteries` variable', function() {
+      expect(totalBatteries).to.exist;
+    });
+
+    it('should have a number as a result', function() {
+      expect(totalBatteries).to.be.a('number');
+    });
+
+    it('should have made the sum of all the assembled batteries', function() {
+      expect(totalBatteries).to.eql(31);
+    });
+  });
+});
+*/
